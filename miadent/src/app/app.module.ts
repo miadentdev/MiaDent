f5ca2ed10e0634e1aacc90e08e72d551271ac4b4
MiaDentDev <miadent.dev@gmail.com>
2026-05-01 11:30:42 +0300

    Added initial pages

--- miadent/src/app/app.module.ts
+++ miadent/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ServicesComponent } from './pages/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    ContactComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
