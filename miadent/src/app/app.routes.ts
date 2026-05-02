b979e9141e294bbb8a39dca0fdc3c72bc2be8c80
MiaDentDev <miadent.dev@gmail.com>
2026-05-02 13:55:27 +0300

    Updated routes

--- miadent/src/app/app.routes.ts
+++ miadent/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ServicesPageComponent } from './pages/services-page/services-page.component';
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'home', component: HomePageComponent },
+  { path: 'about', component: AboutPageComponent },
   { path: 'services', component: ServicesPageComponent },
   { path: 'contact', component: ContactPageComponent },
 ];
