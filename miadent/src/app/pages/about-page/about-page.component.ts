7b5a565e43948f5074f31e775e3f3197513f97cd
MiaDentDev <miadent.dev@gmail.com>
2026-05-02 13:53:59 +0300

    Added about page

---
+++ miadent/src/app/pages/about-page/about-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mia-about-page',
+  imports: [],
+  templateUrl: './about-page.component.html',
+  styleUrl: './about-page.component.css',
+})
+export class AboutPageComponent {}
