525494b79d86060a79057ad649ba41c9bcb77d78
MiaDentDev <miadent.dev@gmail.com>
2026-05-02 12:08:49 +0300

    Added logo component

---
+++ miadent/src/app/layout/logo/logo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mia-logo',
+  imports: [],
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.css',
+})
+export class LogoComponent {}
