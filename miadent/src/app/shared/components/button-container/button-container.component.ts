10db39a9fac10a983943a497da80b8d8fa6e6eb8
MiaDentDev <miadent.dev@gmail.com>
2026-05-02 13:59:27 +0300

    Added a button container component

---
+++ miadent/src/app/shared/components/button-container/button-container.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mia-button-container',
+  imports: [],
+  templateUrl: './button-container.component.html',
+  styleUrl: './button-container.component.css',
+})
+export class ButtonContainerComponent {}
