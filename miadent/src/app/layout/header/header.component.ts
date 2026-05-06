26bb6869cb248de5d8c85fca1f7b2c274a7ba89f
MiaDentDev <miadent.dev@gmail.com>
2026-05-06 11:13:00 +0300

    Added mobile header version

--- miadent/src/app/layout/header/header.component.ts
+++ miadent/src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal, WritableSignal } from '@angular/core';
 import { PageNavigationComponent } from '../../navigation/page-navigation/page-navigation.component';
 import { LogoComponent } from '../logo/logo.component';
 import { HeaderButtonsComponent } from './header-buttons/header-buttons.component';
@@ -9,4 +9,13 @@ import { HeaderButtonsComponent } from './header-buttons/header-buttons.componen
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  protected readonly isDesktop: WritableSignal<boolean> = signal<boolean>(
+    window.innerWidth >= 1170
+  );
+
+  @HostListener('window:resize')
+  protected onResize(): void {
+    this.isDesktop.set(window.innerWidth >= 1170);
+  }
+}
